e1a0b1e64ddeda3eb722f02efea7338b812099f7
DilushaKumari <dilushakumaridt@gmail.com>
2020-06-21 13:23:29 +0530

    Add LICENCE file

--- src/main/java/lk/ou/se/test/AppInitializer.java
+++ src/main/java/lk/ou/se/test/AppInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppInitializer {
+public class  AppInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppInitializer.class,args);
